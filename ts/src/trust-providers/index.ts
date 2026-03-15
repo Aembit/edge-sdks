d710f423c385b1cfe9eb74a334d3568f1d6d5488
Kevin Sapp <sapp.kevin@gmail.com>
2026-03-15 10:14:26 +0300

    feat(ts): expose OIDC ID token trust provider in package API

--- ts/src/trust-providers/index.ts
+++ ts/src/trust-providers/index.ts
@@ -1,16 +1,20 @@
 import { createAwsMetadataServiceTrustProvider } from "./aws-metadata-service.js"
 import { createAwsRoleTrustProvider } from "./aws-role.js"
+import { createOidcIdTokenTrustProvider } from "./oidc-id-token.js"
 
 /**
  * Built-in Trust Provider factory functions.
  */
 export const trustProviders = {
   awsMetadataService: createAwsMetadataServiceTrustProvider,
-  awsRole: createAwsRoleTrustProvider
+  awsRole: createAwsRoleTrustProvider,
+  oidcIdToken: createOidcIdTokenTrustProvider
 }
 
 export { createAwsMetadataServiceTrustProvider } from "./aws-metadata-service.js"
 export { createAwsRoleTrustProvider } from "./aws-role.js"
+export { createOidcIdTokenTrustProvider } from "./oidc-id-token.js"
 
 export type { AwsMetadataServiceTrustProviderOptions } from "./aws-metadata-service.js"
 export type { AwsRoleTrustProviderOptions } from "./aws-role.js"
+export type { OidcIdTokenTrustProviderOptions } from "./oidc-id-token.js"
