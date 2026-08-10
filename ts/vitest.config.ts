59b51d83db5cde2ef004433e3966b7053a27104d
David Walker <david.walker@quantifex.com>
2026-08-10 18:15:34 -0400

    SDK-47-SONAR: Fix vitest.config.ts by removing unsupported all property

--- ts/vitest.config.ts
+++ ts/vitest.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
       provider: "v8",
       reporter: ["text", "lcov"],
       reportsDirectory: "coverage",
-      all: true,
       include: ["src/**/*.ts"],
       exclude: ["src/**/*.test.ts", "src/types/**"]
     }
